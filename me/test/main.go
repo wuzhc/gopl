494c4de845c32a943f16b130408f6a5dd50ea711
wuzhc <1716220125@qq.com>
2019-06-05 09:43:58 +0800

    test int type to time.Duration

--- me/test/main.go
+++ me/test/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	var s string = nil
-	fmt.Println(s)
+	var i = 1
+	time.Sleep(time.Duration(i) * time.Second)
+	fmt.Println("timeout")
 }
